ef578449c7cc4e91a24571a3710b5a87b79a7aa8
Baso1st <abdulwanis1@gmail.com>
2021-12-03 17:44:28 -0500

    Adding all the pic assests and expermenting with the audio to see if I can play two tracks at the same time.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,16 +18,36 @@ export class AppComponent implements OnInit, OnChanges {
   }
 
   async onPageClick() {
-    if (this.musicPlayer.playing) {
-      this.musicPlayer.pause();
-    } else {
-      this.musicPlayer.play();
-    }
+    await this.musicPlayer.play();
+    // await this.clipPlayer.play();
+
+
+    // if (!this.musicPlayer.playing) {
+    //   this.musicPlayer.play();
+    // }
+
+    // var snd1  = new Audio();
+    // var src1  = document.createElement("source");
+    // src1.type = "audio/mpeg";
+    // src1.src  = "/assets/music.mp3";
+    // snd1.appendChild(src1);
+
+    // var snd2  = new Audio();
+    // var src2  = document.createElement("source");
+    // src2.type = "audio/wav";
+    // src2.src  = "/assets/1.wav";
+    // snd2.appendChild(src2);
+
+    // snd1.play(); 
+    // snd2.play(); // Now both will play at the same time
+
   }
 
   onSnowFlakeClick() {
     // ToDo: Play one of the voice clips here. 
-    // this.clipPlayer.play()
+    // this.musicPlayer.pause();
+    // this.clipPlayer.play();
+    // this.musicPlayer.play();
   }
 
 }

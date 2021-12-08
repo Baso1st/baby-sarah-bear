b6fb69673fb7ff7accea8802cff391f17a84c790
Muhammad Abdulwanis <Muhammad.Abdulwanis@steeldynamics.com>
2021-12-08 12:36:51 -0500

    Added the collage grid

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -27,14 +27,6 @@ export class AppComponent {
     '12.jpg', '13.jpg', '14.jpg', '15.jpg', '16.jpg'
   ]
 
-  // imageSources = [
-  //   ['1.jpg', '2.jpg', '3.jpg', '4.jpg'],
-  //   ['5.jpg', '6.jpg', '7.jpg','8.jpg'],
-  //   ['9.jpg', '10.jpg', '11.jpg','12.jpg'],
-  //   ['13.jpg', '14.jpg', '15.jpg', '16.jpg']
-  // ]
-
-
   onPageClick() {
     if (this.isMainAudioPlaying) {
       return
